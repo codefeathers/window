ce6d45823f5846423c9a4ffd64e0fc1b7877d39d
Muthu Kumar <muthukumar@thefeathers.in>
2018-05-12 20:52:36 +0530

    [fix] call custom predicate

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const Slider = require('.');
 const a = [ 0, 1, 2, 3, 4, 5 ];
 const b = [ 2, 3, 4 ];
 
-const w = new Slider({ size: 3 });
+const w = new Slider({ size: 3 , predicate: (a, b) => a === b });
 console.log(w.slide(a, b));
